1f41aee41e55f043750b5a20b224f1e19f686b19
Ellis1234 <51514364+Ellis1234@users.noreply.github.com>
2020-07-19 00:56:48 +0800

    Add files via upload

---
+++ book1.sql
@@ -0,0 +1,51 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 5.6.5-m8 : Database - book1
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`book1` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+
+USE `book1`;
+
+/*Table structure for table `books` */
+
+DROP TABLE IF EXISTS `books`;
+
+CREATE TABLE `books` (
+  `bookID` int(10) NOT NULL AUTO_INCREMENT COMMENT '书id',
+  `bookName` varchar(100) NOT NULL COMMENT '书名',
+  `bookCounts` int(11) NOT NULL COMMENT '数量',
+  `detail` varchar(200) NOT NULL COMMENT '描述',
+  KEY `bookID` (`bookID`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+
+/*Data for the table `books` */
+
+insert  into `books`(`bookID`,`bookName`,`bookCounts`,`detail`) values (1,'Java',1,'从入门到放弃1'),(2,'MySQL',10,'从删库到跑路'),(4,'linux',3,'一种操作系统'),(8,'4',4,'4');
+
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `username` varchar(40) COLLATE utf8_bin NOT NULL,
+  `password` varchar(40) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+/*Data for the table `user` */
+
+insert  into `user`(`username`,`password`) values ('asd','asd');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
